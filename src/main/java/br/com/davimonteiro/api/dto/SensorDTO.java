20527e7923281921f92fd0cc21aae5d6849837b1
davimonteiro <davimonteiro.ce@gmail.com>
2023-01-22 23:39:44 +0000

    Add implementation of the filter for statistics

--- src/main/java/br/com/davimonteiro/api/dto/SensorDTO.java
+++ src/main/java/br/com/davimonteiro/api/dto/SensorDTO.java
@@ -10,7 +10,38 @@ import java.util.Map;
 public class SensorDTO {
 
     private Long sensorId;
-    Map<String, DoubleSummaryStatistics> summaryStatistics = new HashMap<>();
+    Map<String, SummaryStatistics> summaryStatistics = new HashMap<>();
+
+    public void setSummaryStatistics(String metricName, DoubleSummaryStatistics summary, String statistic) {
+        SummaryStatistics statistics = new SummaryStatistics();
+
+        switch (statistic) {
+            case "min":
+                statistics.setMin(summary.getMin());
+                break;
+            case "max":
+                statistics.setMax(summary.getMax());
+                break;
+            case "sum":
+                statistics.setSum(summary.getSum());
+                break;
+            case "average":
+                statistics.setAverage(summary.getAverage());
+                break;
+        }
+
+        summaryStatistics.put(metricName, statistics);
+    }
+
+}
+
+
+@Data
+class SummaryStatistics {
+    private Double sum;
+    private Double min;
+    private Double max;
+    private Double average;
 
 }
 
