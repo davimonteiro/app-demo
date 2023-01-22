a3573a01cf8558fa439a163ba8791e94e7062629
davimonteiro <davimonteiro.ce@gmail.com>
2023-01-22 23:33:51 +0000

    Add a findById method with join

--- src/main/java/br/com/davimonteiro/repository/SensorRepository.java
+++ src/main/java/br/com/davimonteiro/repository/SensorRepository.java
@@ -5,14 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
-import java.util.List;
+import java.util.Optional;
 
 public interface SensorRepository extends JpaRepository<Sensor, Long> {
 
-    @Query(value = "select s from Sensor s join Metric m on s.id = m.sensor.id where s.id in (:ids) and m.name in (:metrics) and m.creationDate <= :date")
-    List<Sensor> querySensorsWithCreationDateBefore(@Param("ids") List<Long> ids,
-                                                    @Param("metrics") List<String> metrics,
-                                                    @Param("date") LocalDate date);
+    @Query(value = "select s from Sensor s join fetch s.metrics where s.id = :id")
+    Optional<Sensor> findSensorBy(@Param("id") Long id);
 
 }
