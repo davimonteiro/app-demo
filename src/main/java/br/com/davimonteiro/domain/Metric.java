fc75baecc4b37debc7a80bc8035e790eabb8ba6a
davimonteiro <davimonteiro.ce@gmail.com>
2023-01-22 23:36:44 +0000

    Add a builder class

--- src/main/java/br/com/davimonteiro/domain/Metric.java
+++ src/main/java/br/com/davimonteiro/domain/Metric.java
@@ -2,13 +2,12 @@ package br.com.davimonteiro.domain;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Getter
 @Setter
 @Entity
@@ -24,8 +23,68 @@ public class Metric {
     private LocalDate creationDate;
 
     @JsonBackReference
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sensor")
     private Sensor sensor;
+
+    public Metric() {
+    }
+
+    public Metric(MetricBuilder metricBuilder) {
+        this.id = metricBuilder.id;
+        this.name = metricBuilder.name;
+        this.value = metricBuilder.value;
+        this.unit = metricBuilder.unit;
+        this.creationDate = metricBuilder.creationDate;
+        this.sensor = metricBuilder.sensor;
+    }
+
+    public static class MetricBuilder {
+        private Long id;
+        private String name;
+        private Double value;
+        private String unit;
+        private LocalDate creationDate;
+
+        private Sensor sensor;
+
+        public MetricBuilder() {
+        }
+
+        public MetricBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public MetricBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public MetricBuilder value(Double value) {
+            this.value = value;
+            return this;
+        }
+
+        public MetricBuilder unit(String unit) {
+            this.unit = unit;
+            return this;
+        }
+
+        public MetricBuilder creationDate(LocalDate creationDate) {
+            this.creationDate = creationDate;
+            return this;
+        }
+
+        public MetricBuilder sensor(Sensor sensor) {
+            this.sensor = sensor;
+            return this;
+        }
+
+        public Metric build() {
+            return new Metric(this);
+        }
+
+    }
 
 }
